e2a4c7fac23f26040110fa8dd0223007630750b8
Al Capwn <gadonj18@live.com>
2015-10-24 12:22:53 -0600

    Added score

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ShipController : MonoBehaviour {
 	public GameObject Detonation;
 	public bool alive = true;
+	private int score = 0;
+
+	void OnTriggerEnter(Collider col) {
+		if (alive) {
+			if (col.GetComponent<Collider>().gameObject.GetComponent<CapsuleController> ()) {
+				GameObject.Destroy (col.GetComponent<Collider>().gameObject);
+				score++;
+			}
+		}
+	}
 
 	void OnCollisionEnter(Collision col) {
-		Debug.Log ("Test");
 		if (alive) {
-			if (col.collider.gameObject.GetComponent<CapsuleController> ()) {
-				GameObject.Destroy(col.collider.gameObject);
-			} else if (col.collider.gameObject.name != "Floor") {
+			if (col.collider.gameObject.name != "Floor") {
 				GameObject detonation = Instantiate (Detonation);
 				this.GetComponent<AudioSource> ().Play ();
 				detonation.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
@@ -20,5 +28,9 @@ public class ShipController : MonoBehaviour {
 				alive = false;
 			}
 		}
+	}
+
+	void Update() {
+		GameObject.Find ("Canvas/Score").GetComponent<Text> ().text = "Score: " + score.ToString ();
 	}
 }
