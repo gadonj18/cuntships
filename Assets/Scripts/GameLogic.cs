2ca913587d7e067b047afe28612501019c09c93b
Al Capwn <gadonj18@live.com>
2015-10-25 12:58:27 -0600

    Added effects like engine burn and capsule sounds and shit.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -2,43 +2,4 @@
 using System.Collections;
 
 public class GameLogic : MonoBehaviour {
-	public GameObject ship;
-	private Rigidbody rb;
-	private Vector3 UpDir;
-	private bool UpPressed = false;
-	private bool LeftPressed = false;
-	private bool RightPressed = false;
-
-	void Start() {
-		rb = ship.GetComponent<Rigidbody>();
-	}
-
-	// Update is called once per frame
-	void Update () {
-		UpPressed = false;
-		if(Input.GetKey("w")) {
-			UpPressed = true;
-		}
-		LeftPressed = false;
-		RightPressed = false;
-		if(Input.GetKey ("a")) {
-			LeftPressed = true;
-		} else if(Input.GetKey ("d")) {
-			RightPressed = true;
-		}
-		ship.transform.position = new Vector3(ship.transform.position.x, ship.transform.position.y, 0f);
-	}
-
-	void FixedUpdate () {
-		if(UpPressed) {
-			rb.AddForce(ship.transform.forward * 10f);
-		}
-		if(LeftPressed) {
-			//rb.AddRelativeTorque(new Vector3(0f, -1f, 0f));
-			ship.transform.Rotate(new Vector3(0f, -100f * Time.deltaTime, 0f));
-		} else if(RightPressed) {
-			//rb.AddRelativeTorque(new Vector3(0f, 1f, 0f));
-			ship.transform.Rotate(new Vector3(0f, 100f * Time.deltaTime, 0f));
-		}
-	}
 }
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -3,14 +3,34 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ShipController : MonoBehaviour {
-	public GameObject Detonation;
+    private Rigidbody rb;
+    private Vector3 UpDir;
+    private bool UpPressed = false;
+    private bool LeftPressed = false;
+    private bool RightPressed = false;
+    public GameObject Detonation;
 	public bool alive = true;
 	private int score = 0;
+    public AudioClip CapsuleSound;
+    public AudioClip DetonationSound;
+    private AudioSource Engine;
+    private AudioSource Effects;
+    private GameObject flame;
+
+    void Start()  {
+        AudioSource[] sources = this.GetComponents<AudioSource>();
+        Effects = sources[0];
+        Engine = sources[1];
+        flame = this.transform.FindChild("Flame").gameObject;
+        rb = this.GetComponent<Rigidbody>();
+    }
 
 	void OnTriggerEnter(Collider col) {
 		if (alive) {
-			if (col.GetComponent<Collider>().gameObject.GetComponent<CapsuleController> ()) {
-				GameObject.Destroy (col.GetComponent<Collider>().gameObject);
+			if (col.GetComponent<Collider>().gameObject.GetComponent<CapsuleController> ())  {
+                Effects.clip = CapsuleSound;
+                Effects.Play();
+                GameObject.Destroy (col.GetComponent<Collider>().gameObject);
 				score++;
 			}
 		}
@@ -20,7 +40,8 @@ public class ShipController : MonoBehaviour {
 		if (alive) {
 			if (col.collider.gameObject.name != "Floor") {
 				GameObject detonation = Instantiate (Detonation);
-				this.GetComponent<AudioSource> ().Play ();
+                Effects.clip = DetonationSound;
+                Effects.Play();
 				detonation.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
 				foreach (Renderer renderer in this.GetComponentsInChildren<Renderer>()) {
 					renderer.enabled = false;
@@ -30,7 +51,50 @@ public class ShipController : MonoBehaviour {
 		}
 	}
 
-	void Update() {
-		GameObject.Find ("Canvas/Score").GetComponent<Text> ().text = "Score: " + score.ToString ();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        UpPressed = false;
+        if (Input.GetKey("w"))
+        {
+            UpPressed = true;
+        }
+        LeftPressed = false;
+        RightPressed = false;
+        if (Input.GetKey("a"))
+        {
+            LeftPressed = true;
+        }
+        else if (Input.GetKey("d"))
+        {
+            RightPressed = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0f);
+
+        GameObject.Find("Canvas/Score").GetComponent<Text>().text = "Score: " + score.ToString();
+    }
+
+    void FixedUpdate()
+    {
+        if (UpPressed)
+        {
+            rb.AddForce(this.transform.forward * 10f);
+            flame.GetComponent<ParticleSystem>().enableEmission = true;
+            if(!Engine.isPlaying) Engine.Play();
+        } else
+        {
+            Engine.Stop();
+            flame.GetComponent<ParticleSystem>().enableEmission = false;
+        }
+        if (LeftPressed)
+        {
+            //rb.AddRelativeTorque(new Vector3(0f, -1f, 0f));
+            this.transform.Rotate(new Vector3(0f, -100f * Time.deltaTime, 0f));
+        }
+        else if (RightPressed)
+        {
+            //rb.AddRelativeTorque(new Vector3(0f, 1f, 0f));
+            this.transform.Rotate(new Vector3(0f, 100f * Time.deltaTime, 0f));
+        }
+    }
 }
